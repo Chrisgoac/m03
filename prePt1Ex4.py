c36d0e8e3babe20ff144069e3fa65109df7d3eb5
Christian <chrisgonzaco@gmail.com>
2022-10-04 22:46:14 +0200

    prePt1Ex4 almost done

---
+++ prePt1Ex4.py
@@ -0,0 +1,14 @@
+while True:
+    print("\t**************MENU**************")
+    print("1 - Tabla interés de un capital\n2 - Salir")
+    select = int(input("Selecciona la opción: "))
+
+    if select == 1:
+        # TODO Preguntar tabla
+        print("")
+    elif select == 2:
+        break
+    else:
+        print("Esa opción no es correcta.")
+
+    
