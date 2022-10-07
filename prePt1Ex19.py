637d5babe03f04e1de8fffbbe9a5621a95e924b6
Christian <chrisgonzaco@gmail.com>
2022-10-07 15:57:15 +0000

    prePt1Ex19 done

---
+++ prePt1Ex19.py
@@ -0,0 +1,21 @@
+total = 0
+
+while True:
+    
+    num1 = int(input("Introduce el número 1: "))
+    num2 = int(input("Introduce el número 2: "))
+    
+    if num1 and num2 <= 0:
+        continue
+    
+    break
+
+
+for i in (range(num2)):
+    if i+1 < num2:
+        print(num1, end=" + ")
+    else:
+        print(num1, end=" = ")
+    total += num1
+    
+print(total)
