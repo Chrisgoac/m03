192c36f878d1416daa90015c26762d5d133c4794
Christian <chrisgonzaco@gmail.com>
2022-10-07 15:12:58 +0000

    Some changes maded

--- prePt1Ex15.py
+++ prePt1Ex15.py
@@ -1,15 +1,14 @@
-num = int(input("Escribe un número: "))
-resultado = False
+while True:
+    num = int(input("Escribe un número: "))
+    resultado = False
 
-for e in range(1, num+1):
     if num > 0:
-        for i in range(2, e):
-            if (e % i) == 0:
+        for i in range(2, num):
+            if (num % i) == 0:
                 resultado = True
                 break
             
     if resultado:
-        print(f"El número {e} no es primo")
+        print(f"El número {num} no es primo")
     else:
-        print(f"El número {e} es primo")
-    resultado = False
+        print(f"El número {num} es primo")
--- prePt1Ex16.py
+++ prePt1Ex16.py
@@ -1,15 +1,15 @@
 num = int(input("Escribe un número: "))
 resultado = False
-num_actual = 1
 
-while num > 0:
-    for i in range(2, num_actual):
-        if (num_actual % i) == 0:
-            resultado = True
-            break
-        
-    if not resultado:
-        num -= 1
-        print(f"{num_actual}")
-    num_actual += 1
+for e in range(1, num+1):
+    if num > 0:
+        for i in range(2, e):
+            if (e % i) == 0:
+                resultado = True
+                break
+            
+    if resultado:
+        print(f"El número {e} no es primo")
+    else:
+        print(f"El número {e} es primo")
     resultado = False
