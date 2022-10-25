ce3efd92afbfc584787af45c2db5665f8801699a
Christian <chrisgonzaco@gmail.com>
2022-10-25 19:11:56 +0000

    matrizEx2 and matrizEx3 and matrizEx4 done

--- matrizEx2.py
+++ matrizEx2.py
@@ -1,4 +1,25 @@
 menu = [[1, "Salir del menú"], [2, "Opción 2"], [3, "Opción 3"], [4, "Opción 4"]]
 
-for i in range(len(menu)):
+""" add_num = int(input("Añadir el número de la lista: "))
+add_option = input("Añadir el texto a la lista: ")
+
+menu.append([add_num, add_option]) """
+
+""" for i in range(len(menu)):
     print(str(menu[i][0]), "-", menu[i][1])
+    
+print() """
+
+while True:
+    
+    add_option = input("Añadir el texto a la lista: ")
+    
+    if add_option == "pepe":
+        break
+    
+    menu.append([len(menu)+1, add_option])
+
+for i in range(len(menu)):
+    for j in range(len(menu[i])):
+        print(menu[i][j], end=" - ")
+    print()
---
+++ matrizEx4.py
@@ -0,0 +1,50 @@
+import random
+
+menu = [[0, "Salir del menú"], [1, "Jugar"]]
+
+lista = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
+
+num_x = 0
+
+print("\t...: Menu :...")
+for i in range(len(menu)):
+    print(str(menu[i][0]), "-", menu[i][1])
+    
+print()
+
+while True:
+    select = int(input("Selecciona una opción: "))
+    if select < len(menu) and select >= 0:
+        break
+    print("Número no válido.")
+    
+if select == 0:
+    print("Has salido correctamente del programa.")
+elif select == 1:    
+        while True:
+            for i in range(len(lista)):
+                for j in range(len(lista[i])):
+                    print(lista[i][j], end=" ")
+                print()
+            
+            if num_x == 9:
+                    print("Has ganado :)")
+                    break
+            
+            select_frst = random.randint(0, 2)
+            select_scnd = random.randint(0, 2)
+            
+            if (select_frst > len(menu) or select_frst < 0) or (select_scnd > len(menu) or select_scnd < 0):
+                print("No estás dentro del rango.")
+                continue
+            
+            if lista[select_frst][select_scnd] == "X":
+                print("En esa posición ya hay X\n")
+                continue
+            else:
+                lista[select_frst][select_scnd] = "X"
+                num_x += 1
+            
+            input("\nPresiona enter para continuar...")
+else: 
+    print("Número incorrecto, aunque está validado de antes y no deberias ver nunca este mensaje...")    
