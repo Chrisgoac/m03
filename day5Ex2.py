790bd922bdb14d8ce9939844f273443571e8519c
Christian <chrisgonzaco@gmail.com>
2023-02-16 10:25:20 +0100

    Test commit

--- day5Ex2.py
+++ day5Ex2.py
@@ -3,7 +3,7 @@ lista = [0,1,3,7,9,4,5,5,5,6,9,10,10,0,3,4,5,6]
 cambiado = True
 
 while cambiado:
-    print("While")
+    print("Whilee")
     cambiado = False 
     for i in range(len(lista)-1):        
         if lista[i] < lista[i+1]:
